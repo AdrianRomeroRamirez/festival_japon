2b8e8da19904538548c888003d107a983bfa39ce
Adrian Romero <adriancaef@gmail.com>
2022-03-05 21:18:56 +0100

    hacerla de una sola página

--- src/App.js
+++ src/App.js
@@ -1,32 +1,58 @@
 import logo from "./logo.svg";
+import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { DropdownButton, Dropdown } from "react-bootstrap";
-import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Restaurantes from "./Restaurantes";
 import Exposiciones from "./Exposiciones";
 import Footer from "./footer";
 
 function App() {
+  const [home, setHome] = useState(true);
+  const [restaurantes, setRestaurantes] = useState(false);
+  const [exposiciones, setExposiciones] = useState(false);
+
+  const homeButton = () => {
+    setHome(true);
+    setRestaurantes(false);
+    setExposiciones(false);
+  };
+
+  const restaurantesButton = () => {
+    setHome(false);
+    setRestaurantes(true);
+    setExposiciones(false);
+  };
+
+  const exposicionesButton = () => {
+    setHome(false);
+    setRestaurantes(false);
+    setExposiciones(true);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <div>
           <DropdownButton id="dropdown-basic-button" title="Menu">
-            <Dropdown.Item href="/">Inicio</Dropdown.Item>
-            <Dropdown.Item href="Restaurantes">Restaurantes</Dropdown.Item>
-            <Dropdown.Item href="Exposiciones">Exposiciones</Dropdown.Item>
+            <Dropdown.Item href="" onClick={homeButton}>
+              Inicio
+            </Dropdown.Item>
+            <Dropdown.Item href="" onClick={restaurantesButton}>
+              Restaurantes
+            </Dropdown.Item>
+            <Dropdown.Item href="" onClick={exposicionesButton}>
+              Exposiciones
+            </Dropdown.Item>
           </DropdownButton>
         </div>
         <img alt="logo" src={logo} />
       </header>
       <main className="App-main">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Restaurantes" element={<Restaurantes />} />
-          <Route path="/Exposiciones" element={<Exposiciones />} />
-        </Routes>
+        {home && <Home />}
+        {restaurantes && <Restaurantes />}
+        {exposiciones && <Exposiciones />}
       </main>
       <Footer />
     </div>
